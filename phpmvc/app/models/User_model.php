171284bed1d3ffc17c6b43c13af7406948704936
sulisrosliani04 <203040011@mail.unpas.ac.id>
2022-10-09 21:53:46 -0700

    menyelesaikan video 7 model

---
+++ phpmvc/app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_model
+{
+  private $nama = 'Sulis';
+
+  public function getUser()
+  {
+    return $this->nama;
+  }
+}
--- phpmvc/app/views/templates/header.php
+++ phpmvc/app/views/templates/header.php
@@ -18,10 +18,11 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item active">
-            <a class="nav-link" href="<?= BASEURL; ?>">Home <span class="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?= BASEURL; ?>/about">About</a>
+          <a class="nav-link" href="<?= BASEURL; ?>">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="<?= BASEURL; ?>/mahasiswa">Mahasiswa</a>
+          <a class="nav-link" href="<?= BASEURL; ?>/about">About</a>
+          </li>
           </li>
         </ul>
       </div>
